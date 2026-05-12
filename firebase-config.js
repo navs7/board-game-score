6173bb667d62af86fbd4f43e8638c1b8f638ae5c
navneet7988 <59042892-navneet7988@users.noreply.replit.com>
2026-05-12 08:10:01 +0000

    Initial commit

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getDatabase, ref, set, onValue, get } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCV7xbAOEXQDJrv3wQtprqKFlQc5kzMCiA",
+  authDomain: "board-game-score-b9ad0.firebaseapp.com",
+  projectId: "board-game-score-b9ad0",
+  storageBucket: "board-game-score-b9ad0.firebasestorage.app",
+  messagingSenderId: "505020668714",
+  appId: "1:505020668714:web:a40ade03386ddf6acbdc53",
+  databaseURL: "https://board-game-score-b9ad0-default-rtdb.firebaseio.com",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db, ref, set, onValue, get };
